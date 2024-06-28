0753c13ba85b45e0b50602ef39203311cc753392
Michemor <yatoijema@gmail.com>
2024-06-28 20:49:39 +0300

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# creates a file in /tmp
+
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
