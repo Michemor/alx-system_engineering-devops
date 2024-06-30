b2febe80167db441c9b2ee074bce42ec5798634f
Michemor <yatoijema@gmail.com>
2024-06-30 20:11:04 +0300

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# uses puppet to make changes to local ssh config file
+include 'stdlib'
+
+file_line {'Turn off passwd auth':
+  ensure => present,
+  path   => '/home/.ssh/config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line {'Declare identity file':
+  ensure => present,
+  path   => '/home/.ssh/config',
+  line   => 'IdentityFile /home/.ssh/school',
+}
