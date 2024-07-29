98f2ea4344cd962b91f7c23501a65e3539265830
Michemor <yatoijema@gmail.com>
2024-07-29 20:05:04 +0300

    Correction of "0. Gather data from an API"

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,23 +4,30 @@ fetches data from a REST API
 Manipulates the data for simple display to user
 """
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    employee_id = argv[1]
+    employee_id = int(sys.argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/"
 
-    # url
-    url = 'https://jsonplaceholder.typicode.com/'
+    info = requests.get(url + f"{employee_id}").json()
+    username = info.get("name")
 
-    # obtain users name
-    users = requests.get(url + f'users/{employee_id}').json()
-    name = users.get('name')
+    tasks = requests.get(url + f"{employee_id}/todos").json()
+    done = []
+    undone = []
+    for task in tasks:
+        if task.get("completed") is True:
+            done.append(task.get("title"))
+        else:
+            undone.append(task.get("title"))
 
-    # fetch the todo list
-    todos = requests.get(url + 'todos', params={'userId': employee_id}).json()
+    completed = len(done) + len(undone)
 
-    done = [d.get('title') for d in todos if d.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):".format
+                                        (username,
+                                        len(done),
+                                        completed))
+    [print("\t {}".format(t)) for t in done]
 
-    print(f"Employee {name} is done with tasks({len(done)}/{len(todos)}):")
-    [print(f"\t {title}") for title in done]
