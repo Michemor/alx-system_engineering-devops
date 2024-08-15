2826861e5d3ee69482bbd3d37baec528b2fc54c5
Michemor <yatoijema@gmail.com>
2024-08-15 09:41:13 +0300

    correction for task 0 and 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,21 +10,13 @@ def number_of_subscribers(subreddit):
     """
     queries the Reddit API and returns the number of
     subscribers for a given subreddit
-
-    The subreddit is passed as an argument to this function
-    1. Checks if the subreddit is valid and proceeds otherwise
-    the program returns 0.
-    2. fetch the result and counts the number of subscribers
     """
 
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
-    search_reddit = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     heads = {"User-Agent": "0-subs/1.0 (Python 3.10; Ubuntu 20.04)"}
 
     try:
-        response = requests.get(search_reddit,
+        response = requests.get(url,
                                 headers=heads,
                                 allow_redirects=False)
         result = response.json().get("data")
