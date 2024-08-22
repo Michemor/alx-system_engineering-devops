3e4be10e0ca474c38addfbfe80b6e38ca41d08f4
Michemor <yatoijema@gmail.com>
2024-08-22 21:30:07 +0300

    Correction of "0. Sky is the limit, let's bring that limit higher"

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,12 @@
 # Increase the number of requests handled by nginx server
 
-exec {'fix--for-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/2048/" /etc/default/nginx',
-    path    => '/usr/local/bin/:/bin/'
-    } ->
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-    exec { 'restart-nginx':
-      command => 'nginx restart',
-        path    => '/etc/init.d/'
-        }
+
+exec { 'restart-nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
