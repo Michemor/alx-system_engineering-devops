2f4fe36d7599ab7e9ed9c9028d6a4f4131f7d43f
Michemor <yatoijema@gmail.com>
2024-08-22 21:46:41 +0300

    relationship arrows

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 # Increase the number of requests handled by nginx server
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/2048/" /etc/default/nginx',
+exec { 'nginx-limit':
+  command => 'sed -i "s/15/2048" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
-
-exec { 'restart-nginx':
+-> exec { 'restart-nginx':
   command => 'nginx restart',
-  path    => '/etc/init.d/'
+  path    => '/etc/init.d'
 }
