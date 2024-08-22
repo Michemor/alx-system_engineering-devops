a44150026046d0db65ca15526ec4cee7c0f38738
Michemor <yatoijema@gmail.com>
2024-08-22 22:48:56 +0300

    reverted to previous relationship

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,9 +3,9 @@
 exec { 'nginx-limit':
   command => 'sed -i "s/15/2048" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
--> exec { 'restart-nginx':
+exec { 'restart-nginx':
   command => 'nginx restart',
   path    => '/etc/init.d'
 }
